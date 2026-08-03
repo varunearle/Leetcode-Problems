012b0267af10ba75eff18c6b0e29d6f68de672f8
EARLE VARUN SAI <vsai17886@gmail.com>
2026-08-03 08:15:28 +0530

    Time: 12 ms (86.94%) | Memory: 136.1 MB (83.46%) - LeetSync

---
+++ 1522-stone-game-iii/stone-game-iii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string stoneGameIII(vector<int>& stoneValue) {
+        int n = stoneValue.size();
+        vector<int> dp(n + 1, 0);
+
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = INT_MIN;
+            int sum = 0;
+
+            for (int k = 0; k < 3 && i + k < n; k++) {
+                sum += stoneValue[i + k];
+                dp[i] = max(dp[i], sum - dp[i + k + 1]);
+            }
+        }
+
+        if (dp[0] > 0)
+            return "Alice";
+        else if (dp[0] < 0)
+            return "Bob";
+        else
+            return "Tie";
+    }
+};
